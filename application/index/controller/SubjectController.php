c8a593d7e91543fa05b9e1e887182dd549a6ab22
feng <1589654294@qq.com>
2021-07-25 10:52:58 +0800

    view

---
+++ application/index/controller/SubjectController.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\index\controller;     
+use think\Controller;
+
+class SubjectController extends Controller
+{
+    public function index()
+    {
+        
+        return $this->fetch();
+    }
+     public function add()
+    {
+        
+        return $this->fetch();
+    }
+
+    public function edit()
+    {
+        
+        return $this->fetch();
+    }
+}
+
